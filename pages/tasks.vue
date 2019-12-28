3ef807833795a4241c14add64461490f297df657
Morikiyo Inoha <morikiyo.inoha@gmail.com>
2019-12-28 23:08:40 +0900

    add tasks page

---
+++ pages/tasks.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="row m-4">
+    <div class="col-12">
+      <h2>Task List</h2>
+      <ul class="list-group">
+        <li v-for="task in tasks" v-bind:key="task.id" class="list-group-item">
+          <input v-bind:checked="task.done" type="checkbox" />
+          {{ task.name }}
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  computed: {
+    tasks() {
+      return this.$store.state.tasks.list
+    }
+  }
+}
+</script>
---
+++ store/tasks.js
@@ -0,0 +1,16 @@
+export const state = () => ({
+  list: [
+    {
+      id: 1,
+      name: 'buy the milk',
+      done: false
+    },
+    {
+      id: 2,
+      name: 'buy the Vue.js book',
+      done: true
+    }
+  ]
+})
+
+export const mutations = {}
